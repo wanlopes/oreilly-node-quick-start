c316b50f48281a265e4b01182281b4b649bbe7b2
Wanessa Lopes <wanessaalopes@gmail.com>
2023-12-27 15:30:35 -0700

    Add error mesage

--- App.js
+++ App.js
@@ -63,15 +63,27 @@
 
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const port = 8080;
 const host = "127.0.0.1";
 
 const server = http.createServer((request, response) => {
-  console.log(request.url);
-  fs.readFile("./mods/index.html", (error, html) => {
+  const base = url.parse(request.url);
+  // console.log(base);
+  const fileName = "./mods" + base.pathname;
+
+  fs.readFile(fileName, (error, html) => {
+    if(error) {
+    response.writeHead(404, {
+      'Content-Type': 'text/html'
+    });
+    response.end(`<h1>Not Found</h1>`);
+    }
+
     response.writeHead(200, { "Content-Type": "text/html" });
     response.write(`<script>alert('Hello');</script>`);
+
     response.write(html);
     response.end();
   });
