880aaabc70349f5d2e94e2f3fc285ad05615348c
Wanessa Lopes <wanessaalopes@gmail.com>
2023-12-27 17:12:05 -0700

    Install express

--- App.js
+++ App.js
@@ -92,3 +92,18 @@ const server = http.createServer((request, response) => {
 server.listen(port, host, () => {
   console.log("ready");
 });
+  
+
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('./', (req, res) => {
+  res.send('Works');
+})
+app.get('/test', (req, res) => {
+  res.send('Test Page');
+})
+
+app.listen(port, () => {
+  return console.log('ready and running');
+})
